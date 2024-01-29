565eeea4656bf98d256dd7304dbe6b5f5989734f
VincentCotella <95920704+VincentCotella@users.noreply.github.com>
2024-01-29 14:30:15 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='Todo-Application',  # Replace with your project's name
+    version='1.0',
+    author='VincentCotella',  # Replace with your name or GitHub username
+    license='MIT',
+    long_description=open('README.md').read(),
+)
